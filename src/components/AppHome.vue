3a79021ecd25cf43b281f076f582621982a3b684
yamasakayuma <>
2020-04-17 02:02:16 +0900

    add about component

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,15 +1,18 @@
 <template>
   <section id="home">
     <AppHomeHero/>
+    <AppHomeAbout/>
   </section>
 </template>
 <script>
 import AppHomeHero from './AppHomeHero.vue'
+import AppHomeAbout from './AppHomeAbout.vue'
 
 export default {
   name: 'AppHome',
   components: {
-    AppHomeHero
+    AppHomeHero,
+    AppHomeAbout
   }
 }
 </script>
