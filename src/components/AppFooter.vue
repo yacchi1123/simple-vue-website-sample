5a5f4537e2855090cc4ed78f610281082aad32a3
yamasakayuma <>
2020-05-23 22:32:46 +0900

    フッター追加

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-footer
+    dark
+  >
+    <div class="footer">
+      Copyright test 2020. All right reserved
+    </div>
+  </v-footer>
+</template>
+
+<script>
+import constants from '../common/constants'
+
+export default {
+  data () {
+    return {
+      drawer: false,
+      tab: false,
+      menuItems: constants.menuItems
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  margin: auto;
+}
+</style>
